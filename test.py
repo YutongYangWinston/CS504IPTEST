f15b87761f1cb1263f6fd2ee694a03dc7809cb8d
yutongyang <yangyutong0969@gmail.com>
2020-10-18 07:18:23 -0700

    Final

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 #This is user Yutong Yang
+<<<<<<< HEAD
 #test
 print("Hello I am User Yutong Yang")
 
 #This is Admin Yutong Yang
 print("revers commit")
 print("Hello, This is the really Admin Yutong Yang")
+=======
+>>>>>>> parent of 1ea4aac... test
